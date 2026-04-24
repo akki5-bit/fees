3080647e90ea4b35df4ffe4c8d9bd0f2a31df052
akki5-bit <vpal70087@gmail.com>
2026-04-24 15:02:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -166,14 +166,4 @@ updateTotal();
 }
 
 createCards();
-function togglePassword(){
 
-let pass=document.getElementById("password");
-
-if(pass.type==="password"){
-pass.type="text";
-}else{
-pass.type="password";
-}
-
-}
